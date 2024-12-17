5134f685ab78ca7c795c1ef143cb3d6978ab9d0a
rayvanovos <9025341@student.tcrmbo.nl>
2024-12-17 13:15:35 +0100

    Delete project demon-bank directory

--- project demon-bank/demon.js
+++
@@ -1,9 +0,0 @@
-function showCurrentDate() {
-    const date = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString('nl-NL', options);
-    document.getElementById('dateDisplay').textContent = formattedDate;
-}
-
-
-window.onload = showCurrentDate;
